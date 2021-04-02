d4be139f6e07d00e81dad69ae5d7042b06e87ae5
will_nash <william_nash_nz@yahoo.co.nz>
2021-04-02 15:13:09 +1300

    Add negative comments.

--- message-generator.js
+++ message-generator.js
@@ -12,16 +12,22 @@ function generateMessage(){
     var posStatement1 = ["like you", "love you", "admire you","adore you", "enjoy your company", "want to spend more time with you"];
     var posStatement2 = ["wonderful", "lovely","kind", "beautiful", "friendly","sweet"];
     var posStatement3 = ["you'll always be my friend.", "good things will happen to you.", "all your dreams will come true."];
+    var negStatement1 = ["hate you", "despise you", "loathe you", "can\'t stand you", "really dislike you"];
+    var negStatement2 = ["nasty", "unkind", "stupid", "smelly", "a liar"];
+    var negStatement3 = ["nothing good will ever happen to you", "you will always be unhappy", "no one will be friends with you"];
+    
     if (posOrNeg == 1){
        // console.log(posStatement1.length);
         var s1 = posStatement1[getRandomInt(posStatement1.length)];
         var s2 = posStatement2[getRandomInt(posStatement2.length)];
         var s3 = posStatement3[getRandomInt(posStatement3.length)]
-        var statement = `I ${s1}, because you are ${s2}, and I hope ${s3}`;
 
     } else {
-        var statement = "I have nothin bad to say yet!";
+        var s1 = negStatement1[getRandomInt(negStatement1.length)];
+        var s2 = negStatement2[getRandomInt(negStatement2.length)];
+        var s3 = negStatement3[getRandomInt(negStatement3.length)]
     }
+    var statement = `I ${s1}, because you are ${s2}, and I hope ${s3}.`;
     
     console.log(statement);
 }
